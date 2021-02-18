36cc7a5235e17e4cd08f83bb3b2b1c8bedc616f6
kudpig <shin.kudo.aa@gmail.com>
2021-02-19 02:53:05 +0900

    Initial Commit

---
+++ Delegate-ButtonAction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Delegate-ButtonAction
+//
+//  Created by shinichiro kudo on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
